2ae0e871edc49e680163e9003cefc01bb8e5e603
Zac <dantepsychedelico@gmail.com>
2015-11-30 20:04:57 +0800

    parse facebook href to <a> tag; add slide title class carousel-caption

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('loyoApp')
-.controller('MainCtrl', function ($scope, $http, $window, FB) {
+.controller('MainCtrl', function ($scope, $http, $window, $sce, FB) {
   $scope.window = {
     width: $window.innerWidth,
     height: $window.innerHeight
@@ -12,13 +12,17 @@ angular.module('loyoApp')
     percentPosition: true,
     gutter: 10
   };
+  function parseHtml(ss) {
+    return ss.replace(/(https?:\/\/[^ $]*)/g, '<a href="$1" target="_blank">連接</a>')
+      .replace(/\n/g, '<br>');
+  }
   $http.get('/api/news/facebook/posts')
   .then(function(results) {
     console.log(results.data);
     $scope.fbData = _.map(results.data, function(data) {
       return {
         created_time: new Date(data.created_time),
-        description: (data.description || data.message || '').replace(/\n/g, '<br>'),
+        description: $sce.trustAsHtml(parseHtml(data.description || data.message || '')),
         full_picture: data.full_picture,
         link: data.link,
         name: data.name,
