7eba0b89fb68d5ebb55439edaf441b1e438e3456
Zac <dantepsychedelico@gmail.com>
2015-12-05 22:36:02 +0800

    fixed bug for mulit image insert

--- server/api/pages/pages.controller.js
+++ server/api/pages/pages.controller.js
@@ -10,69 +10,57 @@ var multiparty = require('multiparty'),
 
 exports.saveImage = function(req, res, next) {
   var form = new multiparty.Form(),
-	    chunks = [],
-      idx = 0,
+      chunks = [],
+      add = 0,
       src = req.params.album+'/'+req.params.filename;
-  Q.all([Image.findOne({ src: src }).exec(), 
-    Album.findOne({ album: req.params.album }).exec()])
-    .then(function(results) {
-      var image = results[0];
-      var album = results[1];
-      var add = 0;
-      if (!image) {
-        image = new Image({
-          src: req.params.album+'/'+req.params.filename,
-          album: req.params.album,
-          name: req.params.filename,
-          ts: new Date()
-        });
-        add += 1;
-      }
-      if (!album) {
-        album = new Album({
-          album: req.params.album,
-          count: 0
-        });
-      }
-      form.on('part', function(part) {
-        if (part.filename) part.resume();
-        part.on('data', function(chunk) {
-          chunks[idx] = chunk;
-          idx += 1;
-        });
-        part.on('end', function() {
-          image.data = Buffer.concat(chunks);
-          image.ts = new Date();
-          image.save()
-          .catch(function(error) {
-            console.log(error);
+  form.on('part', function(part) {
+    if (part.filename) part.resume();
+    part.on('data', function(chunk) {
+      chunks.push(chunk);
+    });
+    part.on('end', function() {
+      Image.findOne({ src: src }).exec()
+      .then(function(image) {
+        if (!image) {
+          image = new Image({
+            src: src,
+            album: req.params.album,
+            name: req.params.filename
           });
-          album.coverSrc = image.src;
-          album.ts = new Date();
-          album.count += add;
-          album.save()
-          .catch(function(err) {
-            console.log(err);
-          });
-        });
-        part.on('error', function(err) {
-          console.log('[Err] part error');
-        });
-      });
-
-      form.on('close', function() {
+          add += 1;
+        }
+        image.data = Buffer.concat(chunks);
+        image.ts = new Date();
+        return image.save();
+      })
+      .then(function(result) {
+        return Album.update({
+          album: req.params.album
+        }, {
+          $set: { coverSrc: src, },
+          $inc: { count: add },
+          $currentDate: { ts: true }
+        }, {upsert: true}).exec();
+      })
+      .then(function(results) {
         res.send();
+      })
+      .catch(function(error) {
+        console.log(error.stack);
+        res.status(500).send();
       });
-
-      form.on('error', function(err) {
-        console.log('[Err] form error' + err.stack);
-      });
-      form.parse(req);
-    })
-    .fail(function(err) {
-      console.log(err);
+    });
+    part.on('error', function(err) {
+      console.log('[Err] part error');
       res.status(500).send();
     });
+  });
+
+  form.on('error', function(err) {
+    console.log('[Err] form error' + err.stack);
+    res.status(500).send();
+  });
+  form.parse(req);
 };
 
 exports.getAlbumSummary = function(req, res, next) {
